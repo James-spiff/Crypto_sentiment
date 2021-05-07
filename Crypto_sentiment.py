0a0285bc751a90cb7cfe1dff624992ad94ab8890
James <Ebi James>
2021-05-07 19:07:41 +0100

    added comments

--- Crypto_sentiment.py
+++ Crypto_sentiment.py
@@ -11,6 +11,7 @@ import emot
 import emoji
 from langdetect import detect, detect_langs
 
+#function to get tweets using snscrape
 def get_tweets(text_query,max_results):
     start_time = time.time()
     tweets_added = 0
@@ -30,6 +31,7 @@ def get_tweets(text_query,max_results):
             break
     return tweets_list
 
+#function to clean tweets removing emojis, newlines, twitter handles etc 
 def clean_tweets(text):
 	emoji_pattern = emoji.get_emoji_regexp("en")
 	text = re.sub(emoji_pattern,"",text)
@@ -42,6 +44,7 @@ def clean_tweets(text):
 
 	return text
 
+#function to filter out non english tweets using textblob
 def translate_text(text):
     blob = textblob.TextBlob(text)
     lang = blob.detect_language()
@@ -55,12 +58,13 @@ def translate_text(text):
         text = text
     return text
 
-text_query = 'Doge Coin'
+text_query = 'Doge Coin' #This is the search keyword
 my_tweets = get_tweets(text_query, 200)
 tweets_df = pd.DataFrame(my_tweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
 
 tweets_df['Text'] = tweets_df["Text"].apply(clean_tweets)
 
+#iterates through the dataframe and creates a new column containing the language used in the tweet
 for text in range(len(tweets_df)):
     blob = textblob.TextBlob(tweets_df.loc[text, 'Text'])
     lang = blob.detect_language()
@@ -74,9 +78,11 @@ tweets_df['Polarity'] = tweets_df['Text'].map(lambda tweet: textblob.TextBlob(tw
 #This maps each tweet to the function textblob.TextBlob(tweet).sentiment.polarity to get it's polarity and then puts it into the column Polarity
 tweets_df['Result'] = tweets_df['Polarity'].map(lambda pol: '+' if pol > 0.1 else '-')
 
+#Specifies which tweets are positive(+) or negative(-)
 positive = tweets_df[tweets_df.Result == '+'].count()['Text']
 negative = tweets_df[tweets_df.Result == '-'].count()['Text']
 
+#Visualizes the result as a bar plot
 plt.bar([0, 1], [positive, negative], label=['Positive', 'Negative'], color=['green', 'red']) #[0, 1] for x axis and [positive, negative] for y axis
 plt.legend()
 plt.title(text_query)
